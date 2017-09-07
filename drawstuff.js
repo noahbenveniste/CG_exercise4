c199a214867fd025fe1c61a4c39e0b325038e65d
bwatson <bwatson@ncsu.edu>
2017-09-07 14:40:48 -0400

    debug color interp

--- drawstuff.js
+++ drawstuff.js
@@ -193,7 +193,7 @@ function twoEdgeInterp(imagedata,e1,e2) {
     // fill ending endpoints of edges with overlapping shared Y range
     var endYDiff = e1[1].y - e2[1].y; 
     if (endYDiff > 0) { // e1 has largest max Y
-        var endAtT = endYDiff/(e1[0].y - e2[1].y); // t at largest min Y
+        var endAtT = -endYDiff/(e1[0].y - e2[1].y); // t at largest min Y
         console.log(endAtT);
         e2new[1].x = e2[1].x; // set X at smallest max Y in e2
         e2new[1].y = e2[1].y; // set Y at smallest max Y in e2
@@ -202,7 +202,7 @@ function twoEdgeInterp(imagedata,e1,e2) {
         e1new[1].y = e2new[1].y; // set Y at smallest max Y in e1
         e1new[1].c = e1[0].c.clone().subtract(e1[1].c).scale(endAtT).add(e1[1].c);  // set color in e1
     } else { // end if e1 largest max Y, begin e2 largest max Y
-        var endAtT = -endYDiff/(e2[0].y - e2[1].y); // t at largest min Y
+        var endAtT = endYDiff/(e2[0].y - e2[1].y); // t at largest min Y
         console.log(endAtT);
         e1new[1].x = e1[1].x; // set X at smallest max Y in e1
         e1new[1].y = e1[1].y; // set Y at smallest max Y in e1
