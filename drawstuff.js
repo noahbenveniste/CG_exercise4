793a5c0c92c79e3f4b57bf5decdeff6e67667aec
bwatson <bwatson@ncsu.edu>
2017-09-11 03:41:33 -0400

    debug

--- drawstuff.js
+++ drawstuff.js
@@ -359,9 +359,9 @@ function twoEdgeInterp(imagedata,e1,e2) {
     try {
         const closeEnough = 0.000000001; // a billionth
         var startXComp = e1new[0].x-e2new[0].x; 
-        startXComp = startXComp > closeEnough ? startXComp : 0; 
+        startXComp = (startXComp > closeEnough) ? startXComp : 0; 
         var endXComp = e1new[1].x-e2new[1].x; 
-        endXComp = endXComp > closeEnough ? endXComp : 0;
+        endXComp = (endXComp > closeEnough) ? endXComp : 0;
         console.log("start:" +startXComp+ "end:" +endXComp);
         
         switch(Math.sign(startXComp) + Math.sign(endXComp)) {
