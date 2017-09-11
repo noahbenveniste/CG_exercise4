c4ab18e56a9171652dd3344b8164e18c291846d3
bwatson <bwatson@ncsu.edu>
2017-09-11 03:27:37 -0400

    debug

--- drawstuff.js
+++ drawstuff.js
@@ -359,6 +359,8 @@ function twoEdgeInterp(imagedata,e1,e2) {
         startXComp = startXComp > closeEnough ? startXComp : 0; 
         var endXComp = e1new[1].x-e2new[1].x; 
         endXComp = endXComp > closeEnough ? endXComp : 0;
+        console.log("start:" +startXComp+ "end:" +endXComp);
+        
         switch(Math.sign(startXComp) + Math.sign(endXComp)) {
             case -2: // both e1 endpoints are left of e2
             case -1: // one e1 endpoint left of e2 (other at same loc)
